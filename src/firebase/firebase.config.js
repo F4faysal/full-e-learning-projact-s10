98051f7cf677f398ae034441dd160db55e3ff268
f4faysal <f4faysals@gmai.com.com>
2022-10-24 22:31:44 +0600

    add fier base

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPF_qgpVAMFVvQrN-AOyF90d6nSZ4fpKk",
+  authDomain: "e-learning-5462d.firebaseapp.com",
+  projectId: "e-learning-5462d",
+  storageBucket: "e-learning-5462d.appspot.com",
+  messagingSenderId: "852357742856",
+  appId: "1:852357742856:web:46b75bfea9e59c2528acbd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
