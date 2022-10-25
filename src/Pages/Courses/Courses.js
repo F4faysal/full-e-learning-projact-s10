35623172c45166abddccdca79ba22ba9f6889658
f4faysal <f4faysals@gmai.com.com>
2022-10-26 01:04:15 +0600

    finish cart

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import CourseCard from "../Shared/CourseCard/CourseCard";
+import Manu from "../Shared/CourseCard/Manu";
 
 const Courses = () => {
   const courses = useLoaderData();
 
   return (
-    <div class="grid grid-rows-1 grid-flow-col gap-5 container mx-auto">
-      <div class="row-span-1  ">01</div>
-      <div class="col-span-2  ">
-        <div className="grid grid-cols-1 md:grid-cols-2  justify-center">
+    <div class=" container mx-auto">
+      <div className="grid grid-rows-1 grid-flow-col gap-4">
+        <div>
+          <ul className="menu bg-base-100 w-56 p-2 rounded-box">
+            {courses.map((course) => (
+              <Manu course={course} key={course.id}></Manu>
+            ))}
+          </ul>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-9">
           {courses.map((course) => (
             <CourseCard course={course} key={course.id}></CourseCard>
           ))}
--- src/Pages/Shared/CourseCard/CourseCard.js
+++ src/Pages/Shared/CourseCard/CourseCard.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CourseCard = ({course}) => {
-  const {details , imgUrl , lessons , price , rating , titel} = course
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl ">
-        <div className="card-body">
-          <h2 className="card-title">{titel}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+const CourseCard = ({ course }) => {
+  const { details, imgUrl, lessons, price, rating, titel } = course;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl drop-shadow-2xl mt-4 mb-3">
+      <div className="card-body">
+        <h2 className="card-title">{titel}</h2>
+        <div className="flex">
+          <p><Link>{lessons}</Link> </p>
+          <p>{price}</p>
+          <p>{rating}</p>
         </div>
-        <figure><img src={imgUrl} alt="Shoes" /></figure>
       </div>
-    );
+      <figure className="p-5 ">
+        <img className="rounded-lg" src={imgUrl} alt="Shoes" />
+      </figure>
+    </div>
+  );
 };
 
 export default CourseCard;
